7ac5a1eefa19fe21dd2b6a2b36a0da95f9c770a9
Sagi-Five <sagi18001@gmail.com>
2024-11-12 00:31:56 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
 
 
+  Scanner sc = new Scanner(System.in);
+        int [] arr = new int[2];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Enter any number Matok :) : ");
+        arr[i] = sc.nextInt();
+        }
+        int result = (int) Math.pow(arr[0], arr[1]);
+
+        System.out.println(result);
     }
 }
